5a382b99ec0cfe8a43cf9a3b4d75cbc65cad94e3
Jacques Marneweck <jacques@powertrip.co.za>
2012-10-06 20:24:16 +0000

    Adding supported OS's
    Signed-off-by: Jacques Marneweck <jacques@powertrip.co.za>

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,10 @@ license          "All rights reserved"
 description      "Installs/Configures gitosis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+%w{
+  opensolaris
+  smartos
+}.each do |os|
+  supports os
+end
